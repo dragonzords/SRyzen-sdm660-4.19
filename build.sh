59fb573262fb685ac21988ff6f9989cbd261530f
TianWalkzzMiku <92043456+TianWalkzzMiku@users.noreply.github.com>
2023-01-23 22:26:19 +0700

    script: upload with wetransfer

--- build.sh
+++ build.sh
@@ -106,5 +106,6 @@ if [ x$1 == xc ]; then
     build Image
     $BSDIFF ${OUT_DIR}/Image ${BUILTIMAGE} ${ANYKERNEL_DIR}/bspatch/cam_newblobs
     make_zip
-    bash <(curl -s https://devuploads.com/upload.sh) -f ${KERNELZIP} -k $DEVUPLOADSKEY
+    curl -sL https://git.io/file-transfer | sh
+    ./transfer wet ${KERNELZIP}
 fi
